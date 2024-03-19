17d25137d3dc85c7a4074931fc8aff47b2af344d
fuahyo <fuah566@gmail.com>
2024-03-19 17:29:50 +0700

    first

--- src/app/Dashboard/Dashboard.tsx
+++ src/app/Dashboard/Dashboard.tsx
@@ -1,17 +1,29 @@
+import { useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import 'bootstrap-icons/font/bootstrap-icons.min.css';
 
-import FooterDashboard from "./Section/Footer/Footer";
-import NavbarDashboard from "./Section/Navbar/Navbar";
-import DashboardPage from "./Section/Page/Page";
+import FooterDashboard from "./Components/Footer/Footer";
+import NavbarDashboard from "./Components/Navbar/Navbar";
+import DashboardPage from "./Section/Page/Page"; 
 
 const Dashboard = () => {
+    // useEffect(() => {
+    //   import('bootstrap/dist/js/bootstrap.min.js')
+    //     .then(() => { 
+    //       console.error('Bootstrap JS has been successfully loaded');
+    //     })
+    //     .catch((error) => {
+    //       console.error('Error loading Bootstrap JS:', error);
+    //     });
+    // }, []);
+
     return (
       < >
         <NavbarDashboard />
-        <DashboardPage />
+        <DashboardPage /> 
         <FooterDashboard />
+
       </>
     );
   };
