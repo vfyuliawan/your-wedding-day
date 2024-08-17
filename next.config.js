76c0c1be932eb23389c2bef3424375b30e8e9b97
vicky fadilla <vicky.yuliawan@ist.id>
2024-08-17 15:57:30 +0700

    fixing next config

--- next.config.js
+++ next.config.js
@@ -4,6 +4,7 @@ const nextConfig = {
       ignoreDuringBuilds: true,
     },
     // Add the output option to export the site as static HTML
+    // add this line when deploy to export static page
     // output: 'export',
   };
   
